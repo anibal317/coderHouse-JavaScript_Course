27d76d35cd9299462c53f65cc544a4983ed637e5
Jorge Anibal Sardón <anibal317@gmail.com>
2022-03-16 18:24:32 -0300

    fin fixes

--- 1-Crear un algoritmo utilizando un ciclo/src/js/index.js
+++ 1-Crear un algoritmo utilizando un ciclo/src/js/index.js
@@ -3,10 +3,14 @@ let number = document.getElementById('inputNumber');
 let btnCalcular = document.getElementById('calcular')
 let lblResult = document.getElementById('lblResult');
 let doWhile= document.getElementById('doWhile');
+let lblPar= document.getElementById('pares');
 
 
 //Ejercicio 1
 for (let i = 1; i <= 10; i++) {
+    if(i%2==0){
+        lblPar.innerText += "index "+i+" es par\n";
+    }
     taskOne.innerText += "2 x " + i + "= " + 2 * i + "\n";
 }
 
